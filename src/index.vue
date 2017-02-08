c7f88299ed6d02b683bc4273735ac612f7833dd2
rowanwins <rowanwins@yahoo.com.au>
2017-02-08 20:17:21 +1100

    Try again on the styling

--- src/index.vue
+++ src/index.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <form :action="url" class="dropzone" :id="id"></form>
+  <form :action="url" class="vue-dropzone dropzone" :id="id"></form>
 
 </template>
 
@@ -152,82 +152,94 @@
 
 </script>
 
-<style scoped>
+<style lang="less">
   @import url('../node_modules/dropzone/dist/dropzone.css');
-
-  .dropzone{
+  
+  .vue-dropzone{
     border: 2px solid #E5E5E5;
     font-family: 'Arial', sans-serif;
     letter-spacing: 0.2px;
     color: #777;
     transition: background-color .2s linear;
+    
+    &:hover {
+      background-color: #F6F6F6;
+    }
+
+    i {
+      color: #CCC;
+    }
+
+    .dz-preview {
+
+      .dz-image {
+        border-radius: 0px;
+        &:hover {
+          img {
+            transform: none;
+            -webkit-filter: none;          
+          }
+        }
+      }
+      
+      .dz-details {
+        bottom: 0px;
+        top: 0px;
+        color: white;
+        background-color: rgba(33, 150, 243, 0.8);
+        transition: opacity .2s linear;
+        text-align: left;
+        .dz-filename span, .dz-size span {
+          background-color: transparent;
+        }
+        .dz-filename:not(:hover) span {
+          border: none;
+        }
+        .dz-filename:hover span {
+          background-color: transparent;
+          border: none;
+        }
+      }
+
+      .dz-progress .dz-upload {
+        background: #cccccc; 
+      }
+
+      .dz-remove {
+        position: absolute;
+        z-index: 30;
+        color: white;
+        margin-left: 15px;
+        padding: 10px;
+        top: inherit;
+        bottom: 15px;
+        border: 2px white solid;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        font-weight: 800;
+        letter-spacing: 1.1px;
+        opacity: 0;
+      }
+
+      &:hover {
+        .dz-remove {
+          opacity: 1;
+        }
+      }
+
+      .dz-success-mark, .dz-error-mark {
+        margin-left: auto!important;
+        margin-top: auto!important;
+        width: 100%!important;
+        top: 35%!important;
+        left: 0;
+        i {
+          color: white!important;
+          font-size: 5rem!important;        
+        }
+      }   
+    }
   }
 
-  .dropzone:hover{
-    background-color: #F6F6F6;
-  }
-
-  .dropzone i{
-    color: #CCC;
-  }
-
-  .dropzone .dz-preview .dz-image {
-    border-radius: 0px;
-  }
-
-  .dropzone .dz-preview:hover .dz-image img{
-    transform: none;
-    -webkit-filter: none;
-  }
-
-  .dropzone .dz-preview .dz-details{
-    bottom: 0px;
-    top: 0px;
-    color: white;
-    background-color: rgba(33, 150, 243, 0.8);
-    transition: opacity .2s linear;
-    text-align: left;
-  }
-  .dropzone .dz-preview .dz-details .dz-filename span, .dropzone .dz-preview .dz-details .dz-size span {
-    background-color: transparent;
-  }
-  .dropzone .dz-preview .dz-details .dz-filename:not(:hover) span{
-    border: none;
-  }
-
-  .dropzone .dz-preview .dz-details .dz-filename:hover span{
-    background-color: transparent;
-    border: none;
-  }
-
-  .dropzone .dz-preview .dz-remove{
-    position: absolute;
-    z-index: 30;
-    color: white;
-    margin-left: 15px;
-    padding: 10px;
-    top: inherit;
-    bottom: 15px;
-    border: 2px white solid;
-    text-decoration: none;
-    text-transform: uppercase;
-    font-size: 0.8rem;
-    font-weight: 800;
-    letter-spacing: 1.1px;
-    opacity: 0;
-  }
-
-  .dropzone .dz-preview:hover .dz-remove {
-    opacity: 1;
-  }
-
-  .dropzone .dz-preview .dz-success-mark, .dropzone .dz-preview .dz-error-mark {
-    margin-left: -40px;
-    margin-top: -50px;
-  }
-
-  .dropzone .dz-preview .dz-success-mark i, .dropzone .dz-preview .dz-error-mark i {
-    color: white;
-    font-size: 5rem;
-  }
 </style>
