e4895a20d4f393e7ada1c39eae28bc4f5992abeb
Alex Bikazzan <alex.bikazzan@gmail.com>
2018-06-27 12:52:37 +0200

    Updated docs and added correct classnames

--- src/components/vue-dropzone.vue
+++ src/components/vue-dropzone.vue
@@ -1,6 +1,8 @@
 <template>
   <div v-bind:class="{ 'vue-dropzone dropzone': includeStyling }" :id="id" ref="dropzoneElement">
-    <slot></slot>
+    <div class="dz-message">
+      <slot>Drop files here to upload</slot>
+    </div>
   </div>
 </template>
 
