82d33bec379e4e8c53675b1e5af3adb6be173831
G Araya <gilber@arayas.com>
2019-09-04 12:21:08 -0400

    Added typing information.

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+import Vue, { Component } from 'vue'
+
+export default class VueDropZone extends Vue {
+
+}
+
+
+
+export interface DropZoneOptions {
+    url?: string | null;
+    method?: string;
+    withCredentials?: boolean;
+    timeout?: number;
+    parallelUploads?: number;
+    uploadMultiple?: boolean;
+    chunking?: boolean;
+    forceChunking?: boolean;
+    chunkSize?: number;
+    parallelChunkUploads?: boolean;
+    retryChunks?: boolean;
+    retryChunksLimit?: number;
+    maxFilesize?: number;
+    paramName?: string;
+    createImageThumbnails?: boolean;
+    maxThumbnailFilesize?: number;
+    thumbnailWidth?: number;
+    thumbnailHeight?: number;
+    thumbnailMethod?: string;
+    resizeWidth?: number | null;
+    resizeHeight?: number | null;
+    resizeMimeType?: string | null;
+    resizeQuality?: number;
+    resizeMethod?: string;
+    filesizeBase?: number;
+    maxFiles?: number | null;
+    headers?: null | {[h: string]: string};
+    clickable?: boolean | string | HTMLElement;
+    ignoreHiddenFiles?: boolean;
+    acceptedFiles?: null | string;
+    autoProcessQueue?: boolean;
+    autoQueue?: boolean;
+    addRemoveLinks?: boolean;
+    previewsContainer?: null | string | HTMLElement;
+    hiddenInputContainer?: null | string | HTMLElement;
+    capture?: null | 'camera' | 'microphone' | 'camcorder';
+    renameFile?: null | ((f: File) => void);
+    forceFallback?: boolean;
+}
