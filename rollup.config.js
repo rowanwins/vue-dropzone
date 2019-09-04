fc94563dbcb25b1d9d455509429c2b7206f44509
G Araya <gilber@arayas.com>
2019-09-04 12:20:54 -0400

    Added ECMAScript 6 module distribution build.

--- rollup.config.js
+++ rollup.config.js
@@ -5,13 +5,25 @@ import replace from 'rollup-plugin-replace';
 import { terser } from "rollup-plugin-terser";
 import css from 'rollup-plugin-css-only'
 
+
+let format = 'umd';
+let output = 'dist/vue2Dropzone.js';
+if (process.env.output == 'esm') {
+    format = 'esm';
+    output = 'dist/vue2Dropzone.esm.js'
+}
+else {
+    // default to umd
+}
+
+
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/vue2Dropzone.js',
+    file: output,
     name: 'vue2Dropzone',
     sourcemap: true,
-    format: 'umd',
+    format: format,
   },
   plugins: [
     resolve({
